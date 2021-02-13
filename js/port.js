000e4aa80680b8803de024fb960d94a8dfd6285e
Hasibul Islam Khan <2016000000032@seu.edu.bd>
2021-02-13 21:50:46 +0600

    first commit

---
+++ js/port.js
@@ -0,0 +1,12 @@
+(function ($) {   
+    /*--------------------------------------
+        mixitup Active
+    ----------------------------------------*/
+var mixer = mixitup('.portfolio_container',{
+    
+    animation:{
+        effectsOut:'fade translateX(-50%)',
+    }
+});
+        
+})(jQuery);	
